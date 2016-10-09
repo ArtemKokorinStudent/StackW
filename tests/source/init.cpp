e6f7b76527dc49d5745b21866dec0665bfe8a114
TrigDeveloper1 <xx.artem-kokorin@mail.ru>
2016-10-09 11:57:24 +0300

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -1,7 +1,3 @@
-#include <catch.hpp>
-
-#include "stack.hpp"
-
 TEST_CASE("Stack can be instantiated by various types", "[instantiation]") {
 	REQUIRE_NOTHROW(stack<int> st1);
 	REQUIRE_NOTHROW(stack<double> st1);
@@ -9,12 +5,13 @@ TEST_CASE("Stack can be instantiated by various types", "[instantiation]") {
 	REQUIRE_NOTHROW(stack<int*> st1);
 	REQUIRE_NOTHROW(stack<stack<int>> st1);
 }
-TEST_CASE("Push, pop", "[push_pop]") {
+TEST_CASE("Push, pop, top", "[push_pop_top]") {
 	stack<int> st;
 	st.push(1);
 	st.push(2);
-	REQUIRE(st.pop() == 2);
-	REQUIRE(st.pop() == 1);
+	REQUIRE(st.top() == 2);
+	st.pop();
+	REQUIRE(st.top() == 1);
 }
 TEST_CASE("count", "[count]") {
 	stack<int> st;
@@ -34,12 +31,15 @@ TEST_CASE("Copy constructor, =", "[copy_ctr, =]") {
 	st1.push(2);
 	REQUIRE_NOTHROW(stack<int> st2 = st1);
 	REQUIRE_NOTHROW(stack<int> st2;
-		st2 = st1);
+	st2 = st1);
 
 	stack<int> st3 = st1;
-	REQUIRE(st1.pop() == 2);
-	REQUIRE(st3.pop() == 2);
+	stack<int> st4 = st1;
+	REQUIRE(st1.top() == 2);
+	REQUIRE(st3.top() == 2);
 	st3.pop();
-	REQUIRE(st3.count() == 0);
-	REQUIRE(st1.pop() == 1);
+	REQUIRE(st3.top() == 1);
+	REQUIRE(st4.top() == 2);
+	st4.pop();
+	REQUIRE(st4.top() == 1);
 }
