346c2e265c853af9a3aecd33767f4fc118cbbe8f
dmred <dmredchenko@gmail.com>
2016-10-21 14:35:55 +0400

    Update stack.hpp

--- include/stack.hpp
+++ include/stack.hpp
@@ -8,7 +8,7 @@ public:
 	stack& operator=(const stack & _stack); /*strong*/
 	size_t count() const; /*noexcept*/
 	void push(T const &); /*strong*/
-	const T& top() const; /*strong*/
+	const T& top() const; /*strong*/ // метод должен возвращать константную ссылку
 	void pop(); /*strong*/
 	bool empty() { return this->count_ == 0; } /*noexcept*/
 	~stack(); /*noexcept*/
@@ -28,14 +28,14 @@ stack<T>::stack(const stack & _stack) /*strong*/
 }
 
 template<typename T>
-stack<T>& stack<T>::operator=(const stack & _stack) /*strong*/
+stack<T>& stack<T>::operator=(const stack & _stack) /*strong*/  //не вызывается очистка памяти ptr_
 {
 	if (this != &_stack) {
 		stack(_stack).swap(*this);
-		/*T* midterm = newCopiedArray(_stack.ptr_, _stack.count_, _stack.size_);
-		delete[] this->ptr_;
-		this->ptr_ = midterm;
-		this->count_ = _stack.count_;
+		/*T* midterm = newCopiedArray(_stack.ptr_, _stack.count_, _stack.size_); // убрать комментарии 
+		delete[] this->ptr_;   
+		this->ptr_ = midterm;  
+		this->count_ = _stack.count_; 
 		this->size_ = _stack.size_;*/
 	}
 	return *this;
@@ -50,7 +50,7 @@ size_t stack<T>::count() const /*noexcept*/
 template<typename T>
 void stack<T>::push(T const & new_element) /*strong*/
 {
-	if (this->count_ >= this->size_) {
+	if (this->count_ >= this->size_) {  //  чисто теоретически count_ может быть  больше чем 1,5 +1 раза больше чем size_ => выделения памяти в след строчки не хватит
 		size_t new_size = (this->size_ * 3) / 2 + 1;
 		T* new_array = operatorNewCopiedArray(this->ptr_, this->count_, new_size);
 		try {
@@ -66,14 +66,14 @@ void stack<T>::push(T const & new_element) /*strong*/
 		this->ptr_ = new_array;
 		this->size_ = new_size;
 	}
-	else {
-		construct(&(this->ptr_[this->count_]), new_element);
+	else {								
+		construct(&(this->ptr_[this->count_]), new_element);		
 	}
 	this->count_++;
 }
 
 template<typename T>
-const T& stack<T>::top() const /*strong*/ //I don't understand first const
+const T& stack<T>::top() const /*strong*/ //I don't understand first const   - конст метод джолжен возвращать конст ссылку
 {
 	if (this->count_ == 0) {
 		throw ("top: count_ == 0");
@@ -87,6 +87,7 @@ void stack<T>::pop() /*strong*/
 	if (this->count_ == 0) {
 		throw("pop(): count_ == 0");
 	}
+	// необходимо очистить память
 	this->count_--;
 }
 
