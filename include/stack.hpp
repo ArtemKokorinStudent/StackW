a84600d636f857eaffc24cc0bb02ad935471cfd7
TrigDeveloper1 <xx.artem-kokorin@mail.ru>
2016-11-27 11:52:09 +0300

    + mutex

--- include/stack.hpp
+++ include/stack.hpp
@@ -1,4 +1,6 @@
 #include "allocator.hpp"
+#include <mutex>
+#include <vector>
 
 template <typename T>
 class stack {
@@ -11,59 +13,73 @@ public:
 
 	void push(T const & value); /*strong*/
 	void pop(); /*strong*/
-	auto top() -> T &; /*strong*/
-	auto top() const -> T const &; /*strong*/
+	auto top()->T &; /*strong*/
+	auto top() const->T const &; /*strong*/
 private:
 	allocator<T> allocator_;
+	std::mutex mutex1;
 };
 
 template<typename T>
 stack<T>::stack(size_t size) : allocator_(size) {
-	; 
+	;
 }
 
 template<typename T>
 auto stack<T>::operator=(stack const & other) -> stack & {
 	if (this != &other) {
+		mutex1.lock();
 		(allocator<T>(other.allocator_)).swap(allocator_);
+		mutex1.unlock();
 	}
 	return *this;
 }
 
 template<typename T>
 void stack<T>::push(T const & value) {
+	mutex1.lock();
 	if (allocator_.full()) {
 		allocator_.resize();
 	}
 	allocator_.construct(this->count(), value);
+	mutex1.unlock();
 }
 
 template<typename T>
 void stack<T>::pop() {
+	mutex1.lock();
 	if (this->count() > 0) {
 		allocator_.destroy(this->count() - 1);
 	}
 	else {
+		mutex1.unlock();
 		throw("stack is empty");
 	}
+	mutex1.unlock();
 }
 
 template<typename T>
 auto stack<T>::top() -> T & {
+	mutex1.lock();
 	if (this->count() > 0) {
+		mutex1.unlock();
 		return allocator_.getElement(this->count() - 1);
 	}
 	else {
+		mutex1.unlock();
 		throw("stack is empty");
 	}
 }
 
 template<typename T>
 auto stack<T>::top() const -> T const & {
+	mutex1.lock();
 	if (this->count() > 0) {
+		mutex1.unlock();
 		return allocator_.getElement(this->count() - 1);
 	}
 	else {
+		mutex1.unlock();
 		throw("stack is empty");
 	}
 }
